d3e401adfa6f3ea4c8d34b98c800a32ee92490de
tigerbluejay <josemariairiarte@gmail.com>
2023-05-04 14:38:58 -0300

    Comments and LINQ Preliminary Functions for Second Part of the Challenge

---
+++ Ejercicio para Developer Net 2/NetExam/Models/Suggestion.cs
@@ -0,0 +1,15 @@
+﻿using NetExam.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetExam.Models
+{
+    public class Suggestion : IOffice
+    {
+        public string LocationName { get; set; }
+        public string Name { get; set; }
+        public int MaxCapacity { get; set; }
+        public IEnumerable<string> AvailableResources { get; }
+    }
+}
